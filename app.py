abae183da639f50374de616f7c70a1128de27356
Cameron-p-Carter <cameron.cz@outlook.com>
2024-05-19 19:46:33 +1000

    ong

--- app.py
+++ app.py
@@ -1,50 +1,62 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import json
 
-
-class User():
+class User:
     def __init__(self, user_name, user_password):
         self._user_name = user_name
         self._user_password = user_password
-        self._flights = []
-   
+        self._flight_booked_ids = []
+
     def getUserName(self):
         return self._user_name
-   
+
     def getUserPassword(self):
         return self._user_password
-   
-    def addFlight(self, x):
-        self._flights.append(x)
 
-    def getFlights(self):
-        return self._flights
+    def addFlightBooking(self, x):
+        self._flight_booked_ids.append(x)
 
-    def getFlight(self, x):
-        return self._flight[x]
+    def getFlightBookingIds(self):
+        return self._flight_booked_ids
 
+    def getFlightBookingId(self, x):
+        return self._flight_booked_ids[x]
 
-class Flight():
-    def __init__(self, flight_name, flight_seat_number, flight_time):
-        self._flight_name = flight_name
-        self._flight_seat_number = flight_seat_number
+class Flight:
+    def __init__(self, flight_id, flight_gate_number, flight_time, flight_departing_location, flight_destination_location, flight_price):
+        self._flight_id = flight_id
+        self._flight_gate_number = flight_gate_number
         self._flight_time = flight_time
+        self._flight_departing_location = flight_departing_location
+        self._flight_destination_location = flight_destination_location
+        self._flight_price = flight_price
+        self._flight_available_seats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-    def getFlightName(self):
-        return self._flight_name
-   
-    def getFlightSeatNumber(self):
-        return self._flight_seat_number
-   
+    def getFlightId(self):
+        return self._flight_id
+
+    def getFlightGateNumber(self):
+        return self._flight_gate_number
+
     def getFlightTime(self):
         return self._flight_time
 
+    def getFlightDepartingLocation(self):
+        return self._flight_departing_location
 
-   
-   
+    def getFlightDestinationLocation(self):
+        return self._flight_destination_location
+
+    def getFlightPrice(self):
+        return self._flight_price
+
+    def getFlightAvailableSeats(self):
+        return self._flight_available_seats
+
 class Manager:
     def __init__(self):
         self._all_users = []
+        self._all_flights = {}
 
     def createUser(self, user_name, user_password):
         new_user = User(user_name, user_password)
@@ -55,110 +67,19 @@ class Manager:
             if user.getUserName() == user_name:
                 return user
         return None
-   
+
     def authenticateUser(self, user_name, user_password):
         for user in self._all_users:
             if user.getUserName() == user_name and user.getUserPassword() == user_password:
                 return user
         return None
-   
-    def addFlightForUser(self, user, flight_name, flight_seat_number, flight_time):
-        new_flight = Flight(flight_name, flight_seat_number, flight_time)
-        user.addFlight(new_flight)
 
-    def loadDataFromJsonFile(self):
-        try:
-            with open('password_data.json', 'r') as file:
-                data = json.load(file)
-                for user_data in data.get('users', []):
-                    user = User(user_data['username'], user_data['password'])
-                    for flight_data in user_data.get('flights', []):
-                        flight = Flight(flight_data['flight_name'], flight_data['username'], flight_data['password'])
-                        user.addFlight(flight)
-                    self._all_users.append(user)
-        except FileNotFoundError:
-            pass
-
-    def saveDataToJsonFile(self):
-        data = {"users": []}
-        for user in self._all_users:
-            user_data = {"username": user.getUserName(), "password": user.getUserPassword(), "flights": []}
-            for flight in user.getFlights():
-                flight_data = {"flight_name": flight.getFlightName(), "username": flight.getFlightSeatNumber(), "password": flight.getFlightTime()}
-                user_data["flights"].append(flight_data)
-            data["users"].append(user_data)
-
-        with open('password_data.json', 'w') as file:
-            json.dump(data, file)
-
-
-
-
-
+    def addFlightForUser(self, user, flight_id):
+        user.addFlight(flight_id)
 
 app = Flask(__name__)
 manager = Manager()
-manager.loadDataFromJsonFile()
 
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-    user = manager.authenticateUser(username, password)
-
-    if user:
-        session['user'] = {
-            'user_name': user.getUserName(),
-            'user_password': user.getUserPassword(),
-            'flights': [{'flight_name': flight.getFlightName(), 'username': flight.getFlightSeatNumber(), 'password': flight.getFlightTime()} for flight in user.getFlights()]
-        }
-        return redirect(url_for('dashboard'))
-    else:
-        return render_template('index.html', error_message="Incorrect username or password")
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        manager.createUser(username, password)
-        manager.saveDataToJsonFile()
-        return redirect(url_for('index'))
-    return render_template('register.html')
-
-
-@app.route('/dashboard', methods=['GET', 'POST'])
-def dashboard():
-    user_data = session.get('user')
-    if not user_data:
-        return redirect(url_for('index'))
-
-    user = User(user_data['user_name'], user_data['user_password'])
-    for flight_data in user_data['flights']:
-        flight = Flight(flight_data['flight_name'], flight_data['username'], flight_data['password'])
-        user.addFlight(flight)
-
-    if request.method == 'POST':
-        flight_name = request.form['flight_name']
-        flight_seat_number = request.form['flight_seat_number']
-        flight_time = request.form['flight_time']
-
-        user.addFlight(Flight(flight_name, flight_seat_number, flight_time))
-
-        existing_user = manager.findUserByUsername(user.getUserName())
-        existing_user.getFlights().append(Flight(flight_name, flight_seat_number, flight_time))
-
-        manager.saveDataToJsonFile()
-
-    return render_template('dashboard.html', user=user)
 
 
 app.secret_key = 'key'
